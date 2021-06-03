bf936491e3739b37271f558eee08384f4b9d8201
MOHAMMED FAIZAN KHAN <1JB17IS044@sjbit.edu.in>
2021-06-03 15:15:26 +0530

    Add project files.

---
+++ Controllers/Product.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D17T1.Controllers
+{
+    public class Product : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public String SI(int id)
+        {
+            int rate = 5;
+            int time = 2;
+            int SI = (id * rate * time) / 100;
+            return "Simple Interest is "+SI;
+        }
+    }
+}
